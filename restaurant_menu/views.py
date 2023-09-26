952fe9cd9f33ea309ad5060e49d61507636ed191
MilanJanko <miljankovic91@gmail.com>
2023-09-26 21:29:24 +0200

    Added Admin interface

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by('date_created')
     template_name = 'index.html'
 
+    def get_context_data(self):
+        context = {'meals': ['Jabuka', 'Pasta']}
+        return context
+
 
 class MenuDetail(generic.DetailView):
     model = Item
