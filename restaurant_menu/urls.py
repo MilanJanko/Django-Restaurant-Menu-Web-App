309f8db5adb3f24b6c4964c3f0457ccaf63915d6
MilanJanko <miljankovic91@gmail.com>
2023-09-26 22:25:11 +0200

    Added dynamic linking

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='home'),
-    path('details/', views.MenuDetail.as_view(), name='menu_detail')
+    path('item/<int:pk>/', views.MenuDetail.as_view(), name='menu_item')
 ]
